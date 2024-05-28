2cc6a4843226e6103ecbfe1a9d63ccb531541762
S <94038358+Kuwax@users.noreply.github.com>
2024-05-29 00:41:12 +0900

    ch3完了

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -1,14 +1,18 @@
 # coding: utf-8
-import sys, os
+import os
+import sys
+
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
-from dataset.mnist import load_mnist
 from PIL import Image
+
+from dataset.mnist import load_mnist
 
 
 def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
+  pil_img = Image.fromarray(np.uint8(img))
+  pil_img.show()
+
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
